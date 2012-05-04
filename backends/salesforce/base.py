1a6ff1d6de3bcb466453df35ff6309b256d213e2
Phil Christensen <pchristensen@freelancersunion.org>
2012-05-04 15:01:14 -0400

    WEB-1919: generating correct query, parse issue still there

--- backends/salesforce/base.py
+++ backends/salesforce/base.py
@@ -1,21 +1,46 @@
 """
-Dummy database backend for Django.
-
-Django uses this if the database ENGINE setting is empty (None or empty string).
-
-Each of these API functions, except connection.close(), raises
-ImproperlyConfigured.
+Salesforce database backend for Django.
 """
+
+import logging
 
 from django.core.exceptions import ImproperlyConfigured
 from django.db.backends import BaseDatabaseWrapper, BaseDatabaseClient, BaseDatabaseIntrospection, BaseDatabaseFeatures, BaseDatabaseValidation
 from django.db.backends.creation import BaseDatabaseCreation
+from django.db.models.sql import compiler
 
 from django.db.backends.postgresql_psycopg2.base import PostgresqlDatabaseOperations
 from django.db.backends.postgresql_psycopg2.base import PostgresqlDatabaseOperations
+
+log = logging.getLogger(__name__)
 
 def complain(*args, **kwargs):
 	raise ImproperlyConfigured("Not yet implemented for the Salesforce backend.")
+
+class SQLCompiler(compiler.SQLCompiler):
+	def get_columns(self, with_aliases=False):
+		cols = compiler.SQLCompiler.get_columns(self, with_aliases)
+		return [x.split('.')[1].strip('"') for x in cols]
+	
+	def get_from_clause(self):
+		result = []
+		first = True
+		for alias in self.query.tables:
+			if not self.query.alias_refcount[alias]:
+				continue
+			try:
+				name, alias, join_type, lhs, lhs_col, col, nullable = self.query.alias_map[alias]
+			except KeyError:
+				# Extra tables can end up in self.tables, but not in the
+				# alias_map if they aren't in a join. That's OK. We skip them.
+				continue
+			if(name.startswith('salesforce_')):
+				name = name[11:]
+				name = ''.join([x.capitalize() for x in name.split('_')])
+			connector = not first and ', ' or ''
+			result.append('%s%s' % (connector, name))
+			first = False
+		return result, []
 
 class DatabaseError(Exception):
 	pass
@@ -84,3 +109,7 @@ class DatabaseWrapper(BaseDatabaseWrapper):
 		self.creation = DatabaseCreation(self)
 		self.introspection = DatabaseIntrospection(self)
 		self.validation = DatabaseValidation(self)
+	
+	def quote_name(self, name):
+		return name # Quoting once is enough.
+
--- models.py
+++ models.py
@@ -11,10 +11,13 @@ import django_roa
 log = logging.getLogger(__name__)
 
 class SalesforceModel(django_roa.Model):
+	class Meta:
+		abstract = True
+	
 	@staticmethod
 	def get_resource_url_list(queryset, server=settings.SF_SERVER):
 		conn = base.DatabaseWrapper()
-		sql, params = compiler.SQLCompiler(queryset.query, conn, None).as_sql()
+		sql, params = base.SQLCompiler(queryset.query, conn, None).as_sql()
 		result = u'%s%s?%s' % (server, '/services/data/v23.0/query', urllib.urlencode(dict(
 			q	= sql % params,
 		)))
