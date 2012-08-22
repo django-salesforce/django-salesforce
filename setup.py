a3054365d66ce8c0323d16e2c9975b533948ccde
Phil Christensen <pchristensen@freelancersunion.org>
2012-08-22 14:33:05 -0400

    bumped version number

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def autosetup():
 	from setuptools import setup, find_packages
 	return setup(
 		name			= "django-salesforce",
-		version			= "0.1.1",
+		version			= "0.1.2",
 		
 		include_package_data = True,
 		zip_safe		= False,
