0eed36c357e84cde47fe92e17019cc03efb7eec4
Phil Christensen <pchristensen@freelancersunion.org>
2012-09-19 10:54:38 -0400

    versioning change in setup

--- setup.py
+++ setup.py
@@ -10,21 +10,41 @@ from __future__ import with_statement
 import distribute_setup
 distribute_setup.use_setuptools()
 
-import os
+import os, os.path, subprocess
 
 # disables creation of .DS_Store files inside tarballs on Mac OS X
 os.environ['COPY_EXTENDED_ATTRIBUTES_DISABLE'] = 'true'
 os.environ['COPYFILE_DISABLE'] = 'true'
 
-def get_requirements(filename='requirements.txt'):
-	with open(os.path.join(os.path.dirname(__file__), filename), 'rU') as f:
-		return f.read().split('\n')
+def relative_path(path):
+	return os.path.join(os.path.dirname(__file__), path)
+
+def get_tagged_version():
+	if(os.path.exists(relative_path('VERSION'))):
+		with open(relative_path('VERSION'), 'rU') as f:
+			version = f.read().strip()
+	else:
+		proc = subprocess.Popen(['git', 'describe', '--tags'],
+			stderr	= subprocess.PIPE,
+			stdout	= subprocess.PIPE,
+			cwd		= os.path.dirname(__file__) or None
+		)
+		(stdoutdata, stderrdata) = proc.communicate()
+		if(proc.returncode):
+			raise RuntimeError(stderrdata)
+		version = stdoutdata.lstrip('v')
+		
+		print "writing version file..."
+		with open(relative_path('VERSION'), 'w') as f:
+			f.write(version)
+	print 'package version: %s' % version
+	return version
 
 def autosetup():
 	from setuptools import setup, find_packages
 	return setup(
 		name			= "django-salesforce",
-		version			= "0.1.3",
+		version			= get_tagged_version(),
 		
 		include_package_data = True,
 		zip_safe		= False,
@@ -36,7 +56,7 @@ def autosetup():
 			],
 		},
 		
-		install_requires = get_requirements(),
+		install_requires = open(relative_path('requirements.txt'), 'rU'),
 		
 		# metadata for upload to PyPI
 		author			= "Phil Christensen",
