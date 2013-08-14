5d4155f5d0b4c590b796e66e97afd2f6ff868626
Phil Christensen <phil@bubblehouse.org>
2013-08-14 19:11:10 -0400

    When I'm stalking other people's forks of this library, I see them do weird things because they don't trust my setup.py file. Trust me, it's awesome ;-)

--- setup.py
+++ setup.py
@@ -17,9 +17,28 @@ os.environ['COPY_EXTENDED_ATTRIBUTES_DISABLE'] = 'true'
 os.environ['COPYFILE_DISABLE'] = 'true'
 
 def relative_path(path):
+	"""
+	Return the given path relative to this file.
+	"""
 	return os.path.join(os.path.dirname(__file__), path)
 
 def get_tagged_version():
+	"""
+	Determine the current version of this package.
+	
+	We tag all our releases in Git, but we want to be able to distribute
+	source packages without having to modify setup.py (and obviously without
+	including an entire Git history).
+	
+	Since we're always creating the sdist from a checked-out Git repo, we
+	let setup.py query Git and save it into a VERSION file. All subsequent
+	runs of setup.py will read version info out of that file, and not
+	attempt to read the current Git tag.
+	
+	It's therefore important that VERSION never gets checked into Git, and
+	that you delete VERSION after creating a source package (or at least
+	before making another one for a new version).
+	"""
 	if(os.path.exists(relative_path('VERSION'))):
 		with open(relative_path('VERSION'), 'rU') as f:
 			version = f.read().strip()
@@ -50,14 +69,21 @@ def autosetup():
 		zip_safe		= False,
 		packages		= find_packages(),
 
+		# setuptools won't auto-detect Git managed files without this
 		setup_requires = [ "setuptools_git >= 0.4.2", ],
 		
 		entry_points	= {
+			# Dependencies suck, but this is definitely preferable
+			# to writing everything into MANIFEST.in by hand.
 			'setuptools.file_finders'	: [
 				'git = setuptools_git:gitlsfiles',
 			],
 		},
 		
+		# okay, so, normally install_requires is a list, right? Well, setuptools
+		# really just iterates over it. As it happens, the pip requirements file
+		# is in the same format, *and* file objects in python allow you to iterate
+		# over them, getting back each line in turn. Why doesn't everyone do this?
 		install_requires = open(relative_path('requirements.txt'), 'rU'),
 		
 		# metadata for upload to PyPI
