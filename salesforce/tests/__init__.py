4d21bdbca7669867e153327c80bce0249aac93c4
Hynek Cernoch <hynek@sdb.cz>
2013-08-15 13:57:15 +0200

    Fixed selection of individual tests on command line.
    It is more important than specifying the order of test cases (the order will
    be undefined after this fix).

--- salesforce/tests/__init__.py
+++ salesforce/tests/__init__.py
@@ -1,12 +1,2 @@
-from django.utils import unittest
-
-def load_test_suite(package_name):
-	suite = unittest.TestSuite()
-	loader = unittest.TestLoader()
-
-	for test in loader.discover(package_name):
-		suite.addTest(test)
-
-	return lambda: suite
-
-suite = load_test_suite(__name__)
+from salesforce.tests.test_auth import *
+from salesforce.tests.test_integration import *
