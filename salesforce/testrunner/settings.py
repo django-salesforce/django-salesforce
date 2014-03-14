606c9754819f370418da4e4631b7345c5f272d1e
Phil Christensen <pchristensen@freelancersunion.org>
2014-03-14 12:41:03 -0400

    wish i could figure out a better way to get this info to travis

--- salesforce/testrunner/settings.py
+++ salesforce/testrunner/settings.py
@@ -20,10 +20,10 @@ DATABASES = {
 	# in order to protect private secret values from unintentional committing.
 	'salesforce': {
 		'ENGINE': 'salesforce.backend',
-		"CONSUMER_KEY" : '',
-		"CONSUMER_SECRET" : '',
-		'USER': '',
-		'PASSWORD': '',
+		"CONSUMER_KEY" : '3MVG9JZ_r.QzrS7icihiIU063YpiIRjEFszVnMCZZmLC5KORG6fhJIxw5ahqgV3lDDCrV..PyA8Jr5JT4JzQk',
+		"CONSUMER_SECRET" : '228684408521557504',
+		'USER': 'djangosalesforce@gmail.com',
+		'PASSWORD': 'Cb6>pd8d-A',
 		'HOST': 'https://test.salesforce.com',
 	}
 }
