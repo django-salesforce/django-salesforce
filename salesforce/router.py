ea8f33a9877740a1a72fd3d613b243d3f6e63939
Hynek Cernoch <hynek@sdb.cz>
2014-05-12 00:04:08 +0200

    Preparing for multi db

--- salesforce/router.py
+++ salesforce/router.py
@@ -15,11 +15,14 @@ from django.conf import settings
 
 log = logging.getLogger(__name__)
 
-def is_testing(db):
-	# The strange name "is_testing" means "this db alias doesn't belong to a SF database"
+def is_sf_database(db):
+	"""The alias is a Salesforce database."""
 	from django.db import connections
 	from salesforce.backend.base import DatabaseWrapper 
-	return not isinstance(connections[db], DatabaseWrapper)
+	return isinstance(connections[db], DatabaseWrapper)
+
+#def is_testing(db):
+#	return not is_sf_database(db)
 
 class ModelRouter(object):
 	"""
@@ -29,27 +32,38 @@ class ModelRouter(object):
 	
 	def db_for_read(self, model, **hints):
 		"""
-		If passed a class or instance, return the salesforce alias if it's a subclass of SalesforceModel.
+		If given some hints['instance'] that is saved in a db, use related
+		fields from the same db. Otherwise if passed a class or instance to
+		model, return the salesforce alias if it's a subclass of SalesforceModel.
 		"""
-		from salesforce import models
+		if 'instance' in hints:
+			db = hints['instance']._state.db
+			if db:
+				return db
 		if getattr(model, '_salesforce_object', False):
 			return self.sf_alias
 
 	def db_for_write(self, model, **hints):
 		"""
-		If passed a class or instance, return the salesforce alias if it's a subclass of SalesforceModel.
+		If given some hints['instance'] that is saved in a db, use related
+		fields from the same db. Otherwise if passed a class or instance to
+		model, return the salesforce alias if it's a subclass of SalesforceModel.
 		"""
-		from salesforce import models
+		if 'instance' in hints:
+			db = hints['instance']._state.db
+			if db:
+				return db
 		if getattr(model, '_salesforce_object', False):
 			return self.sf_alias
 
 	def allow_syncdb(self, db, model):
 		"""
-		Don't attempt to sync salesforce models.
+		Don't attempt to sync salesforce models or db.
 		"""
+		from django.db import connections
 		if(is_testing(db)):
 			return True
-		if(db == self.sf_alias):
+		if(connections[db].settings_dict['ENGINE'] == 'salesforce.backend'):
 			return False
 		if(hasattr(model, '_salesforce_object')):
 			return False
