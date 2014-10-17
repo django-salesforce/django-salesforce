2fbde6f4f3894ae6c94ac1b93e95c5db461b243e
Hynek Cernoch <hynek@sdb.cz>
2014-10-17 13:33:22 +0200

    Added tests for multiple inheritance (abstract models, proxy, mixins).

---
+++ tests/test_mixin/test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+DJANGO_VER=$(python -c "import django; print(django.get_version())")
+if ! [ $DJANGO_VER '<' 1.6 ]; then
+	TEST_BASE=tests.
+fi
+python manage.py test --settings=tests.test_mixin.settings ${TEST_BASE}test_mixin
