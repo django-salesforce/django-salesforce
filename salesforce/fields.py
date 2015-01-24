486dcb8cbce64d4ea6ec8e0922329da3532ed898
Hynek Cernoch <hynek@sdb.cz>
2015-01-24 02:10:16 +0100

    Simplified SalesforceAutoField because many methods can be easily inherited.

--- salesforce/fields.py
+++ salesforce/fields.py
@@ -44,34 +44,24 @@ if not SF_PK in ('id', 'Id'):
 class SalesforceAutoField(fields.AutoField):
 	"""
 	An AutoField that works with Salesforce primary keys.
+
+	It is used by SalesforceModel as a custom primary key. It doesn't convert
+	its value to int.
 	"""
 	description = _("Text")
-	
-	empty_strings_allowed = True
+
 	default_error_messages = {
 		'invalid': _('This value must be a valid Salesforce ID.'),
 	}
-	def __init__(self, *args, **kwargs):
-		assert kwargs.get('primary_key', False) is True, "%ss must have primary_key=True." % self.__class__.__name__
-		kwargs['blank'] = True
-		kwargs['null'] = False
-		kwargs['default'] = None
-		fields.Field.__init__(self, *args, **kwargs)
-	
-	def get_internal_type(self):
-		return "AutoField"
-	
+
 	def to_python(self, value):
 		if isinstance(value, string_types) or value is None:
 			return value
 		return smart_text(value)
-	
-	def validate(self, value, model_instance):
-		pass
-	
+
 	def get_prep_value(self, value):
 		return self.to_python(value)
-	
+
 	def contribute_to_class(self, cls, name):
 		name = name if self.name is None else self.name
 		if name != SF_PK or not self.primary_key:
@@ -91,9 +81,7 @@ class SalesforceAutoField(fields.AutoField):
 		super(SalesforceAutoField, self).contribute_to_class(cls, name)
 		cls._meta.has_auto_field = True
 		cls._meta.auto_field = self
-	
-	def formfield(self, **kwargs):
-		return None
+
 
 class SfField(models.Field):
 	"""
