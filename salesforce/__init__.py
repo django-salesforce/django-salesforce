9a197de93e68a1493e0682c8eca7f67216af31b6
Hynek Cernoch <hynek@sdb.cz>
2015-04-27 17:25:41 +0200

    Added RuntimeWarning about an incomplete support for Django 1.8

--- salesforce/__init__.py
+++ salesforce/__init__.py
@@ -12,6 +12,7 @@ Allows access to all Salesforce objects accessible via the SOQL API.
 """
 import logging
 import ssl
+import warnings
 
 import django
 DJANGO_15_PLUS = django.VERSION[:2] >= (1, 5)
@@ -20,6 +21,10 @@ DJANGO_17_PLUS = django.VERSION[:2] >= (1, 7)
 DJANGO_18_PLUS = django.VERSION[:2] >= (1, 8)
 if not django.VERSION[:2] >= (1, 4):
 	raise ImportError("Django 1.4 or higher is required for django-salesforce.")
+if django.VERSION[:2] >= (1, 8):
+	warnings.warn("The support for Django 1.8 is currently INCOMPLETE in django-salesforce. "
+				"Use Django 1.7 or read the details in README and test your app "
+				"carefully with Django 1.8")
 
 log = logging.getLogger(__name__)
 
