74741d3e1975ce7fe0cc881d6517909c4287120f
Hynek Cernoch <hynek@sdb.cz>
2015-08-02 23:55:34 +0200

    Fixed SslTest for clear suggestion texts after fail.
    All SSL tests passed on Fedora 22 Live-DVD after updating only "openssl"
    package, for both Python 2 and Python 3

--- salesforce/tests/test_ssl.py
+++ salesforce/tests/test_ssl.py
@@ -47,7 +47,27 @@ class SslTest(TestCase):
 		except requests.exceptions.SSLError as e:
 			exc = e
 		if exc:
-			raise type(exc)(exc.args[0], "Can not connect to server with disabled TLS 1.0")
+			messages = ["Can not connect to server with disabled TLS 1.0"]
+			response = None
+			if SslHttpAdapter().sf_ssl_version != ssl.PROTOCOL_SSLv23:
+				adapter = SslHttpAdapter(ssl_version=ssl.PROTOCOL_SSLv23)
+				session = requests.Session()
+				session.mount('https://', adapter)
+				try:
+					response = session.get('https://tls1test.salesforce.com')
+				except requests.exceptions.SSLError as e:
+					pass
+			if response:
+				messages.append("This can be fixed by setting "
+					"\"import ssl; SF_SSL = {'ssl_version': ssl.PROTOCOL_SSLv23}\" ")
+				if self.verify_rejected_ssl("https://ssl3.zmap.io/sslv3test.js"):
+					messages.append("The setting should be fixed just now.")
+				else:
+					messages.append("but this would enable the insecure SSLv3 protocol")
+			else:
+				messages.append("The system should be updated to enable newer TLS protocols.")
+			messages.append("see README")
+			raise type(exc)(exc.args[0], '\n' + '\n'.join(messages))
 		self.assertIn('TLS 1.0 Deactivation Test Passed', response.text)
 
 	def test_under_downgrade_attack_to_ssl_3(self):
@@ -58,20 +78,8 @@ class SslTest(TestCase):
 		# https://zmap.io/sslv3/sslv3test.html
 		url = "https://ssl3.zmap.io/sslv3test.js"
 		if not self.verify_rejected_ssl(url):
-			raise Exception("The protocol SSLv3 should be disabled. see README")
-		if SslHttpAdapter().sf_ssl_version == ssl.PROTOCOL_TLSv1:
-			adapter = SslHttpAdapter(ssl_version=ssl.PROTOCOL_SSLv23)
-			session = requests.Session()
-			session.mount('https://', adapter)
-			try:
-				response = session.get(url)
-				print("It is important to UPGRADE Python and/or SSL before you "
-					"can enable automatic selection of the highest protocol, "
-					"but you must catch both before February 2016. see README")
-			except requests.exceptions.SSLError as e:
-				print("Test passed that you can update settings now to "
-					"\"import ssl; SF_SSL = {'ssl_version': ssl.PROTOCOL_SSLv23}\" "
-					"in order to use new TLS protocols. see README") 
+			raise Exception("The protocol SSLv3 should be disabled for better security "
+					"if possible. (It is disabled on all new systems.) see README")
 
 	# all ssllabs tests are from https://www.ssllabs.com/ssltest/viewMyClient.html
 	@skipUnless(explicitely_selected, "these tests can be enabled on command line")
