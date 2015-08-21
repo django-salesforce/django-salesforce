7dd0c82218e29072fc0a2bbe4b5d805f2c1c1f34
Hynek Cernoch <hynek@sdb.cz>
2015-08-21 19:29:15 +0200

    Implemented conditional expectedFailureIf decorator, with a test

--- salesforce/backend/test_helpers.py
+++ salesforce/backend/test_helpers.py
@@ -16,3 +16,10 @@ uid = '-' + str(uuid.uuid4())[:7]
 sf_alias = getattr(settings, 'SALESFORCE_DB_ALIAS', 'salesforce')
 default_is_sf = router.is_sf_database(sf_alias)
 current_user = settings.DATABASES[sf_alias]['USER']
+
+def expectedFailureIf(condition):
+	"""Conditional 'expectedFailure' decorator for TestCase"""
+	if condition:
+		return expectedFailure
+	else:
+		return lambda func: func
