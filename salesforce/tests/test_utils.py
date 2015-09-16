c2484a198c9f3ff3c21f7f2aae13b39f49431b18
Hynek Cernoch <hynek@sdb.cz>
2015-09-16 20:48:09 +0200

    Implemented advanced parameters to convert_lead.
    It allows to merge Lead with an existing Account etc.

--- salesforce/tests/test_utils.py
+++ salesforce/tests/test_utils.py
@@ -21,18 +21,31 @@ class UtilitiesTest(TestCase):
     def test_lead_conversion(self):
         """
         Create a Lead object within Salesforce and try to
-        convert it, then clean all the generated objects.
+		convert it, convert/merge it with the information from a duplicit Lead,
+        then clean all the generated objects.
         """
-        lead = Lead(FirstName="Foo", LastName="Bar", Company="django-salesforce")
+        lead = Lead(FirstName="Foo", LastName="Bar", Company="django-salesforce",
+                    Street='Test Avenue 45')
         lead.save()
+        lead2 = Lead(FirstName="Foo", LastName="Bar", Company="django-salesforce",
+                     Phone='123456789')
+        lead2.save()
         ret = None
         try:
-            ret = convert_lead(lead)
-            print("Response from convertLead: " +
-                    ', '.join('%s: %s' % (k, v) for k, v in sorted(ret.items())))
+            # convert the first Lead
+            ret = convert_lead(lead, doNotCreateOpportunity=True)
+            #print("Response from convertLead: " +
+            #        ', '.join('%s: %s' % (k, v) for k, v in sorted(ret.items())))
             expected_names = set(('accountId', 'contactId', 'leadId', 'opportunityId', 'success'))
             self.assertEqual(set(ret), expected_names)
             self.assertEqual(ret['success'], 'true')
+            # merge the new Account with the second Lead
+            ret2 = convert_lead(lead2, doNotCreateOpportunity=True, accountId=ret['accountId'])
+            account = Account.objects.get(pk=ret['accountId'])
+            # verify that account is merged
+            self.assertEqual(ret2['accountId'], account.Id)
+            self.assertEqual(account.BillingStreet, 'Test Avenue 45')
+            self.assertEqual(account.Phone, '123456789')
         finally:
             # Cleaning up...
             if ret:
@@ -41,3 +54,4 @@ class UtilitiesTest(TestCase):
                 account = Account.objects.get(pk=ret['accountId'])
                 account.delete()
             lead.delete()   # FYI, ret['leadId'] == lead.pk
+            lead2.delete()
