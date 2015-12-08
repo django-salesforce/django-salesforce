7951cb5974e3041f75eef1bf2fa214a76ab6a2cc
Hynek Cernoch <hynek@sdb.cz>
2015-12-08 04:56:00 +0100

    Mitigated the initial warning for Django 1.8 and 1.9.

--- salesforce/__init__.py
+++ salesforce/__init__.py
@@ -20,7 +20,7 @@ DJANGO_19_PLUS = django.VERSION[:3] >= (1, 9)
 if not django.VERSION[:2] >= (1, 7):
 	raise ImportError("Django 1.7 or higher is required for django-salesforce.")
 if django.VERSION[:2] >= (1, 8):
-	warnings.warn("Some methods for Django 1.8 and 1.9 that work with Django 1.7 are still INCOMPLETE. "
-				  "See the django-salesforce README")
+	warnings.warn("Some methods working with Django 1.7 can be unimplemented for Django 1.8 and 1.9. "
+				  "See the django-salesforce README, the first pragraph")
 
 log = logging.getLogger(__name__)
