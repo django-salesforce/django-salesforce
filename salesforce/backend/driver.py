f4e783324997cd1575c4af9aaaad58bf3c71674e
zee93 <ahmeds.elghareeb@gmail.com>
2017-04-13 18:50:25 +0200

    retry on ConnectionError instead of ConnectionResetError

--- salesforce/backend/driver.py
+++ salesforce/backend/driver.py
@@ -144,11 +144,11 @@ def handle_api_exceptions(url, f, *args, **kwargs):
     # TODO some timeouts can be rarely raised as "SSLError: The read operation timed out"
     except requests.exceptions.Timeout:
         raise SalesforceError("Timeout, URL=%s" % url)
-    except ConnectionResetError:
+    except ConnectionError:
         # retry two more times
         try:
             response = f(url, *args, **kwargs_in)
-        except ConnectionResetError:
+        except ConnectionError:
             response = f(url, *args, **kwargs_in)
 
     if response.status_code == 401:
