51a25087af8c5b8561dc7772fa14cf1b7723b3d1
Hynek Cernoch <hynek@sdb.cz>
2017-04-30 18:29:06 +0200

    Updated coverage_run

--- coverage_run.sh
+++ coverage_run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # expects that "tox" has been run
-.tox/py35-dj110/bin/coverage run --source salesforce manage.py validate >/dev/null
-for x in py34-dj18 py27-dj19 py35-dj110; do
+.tox/py36-dj111/bin/coverage run --source salesforce manage.py check >/dev/null
+for x in py27-dj18 py34-dj19 py35-dj110 py36-dj111; do
     .tox/${x}/bin/coverage run -a --source salesforce manage.py inspectdb --database=salesforce >/dev/null
     .tox/${x}/bin/coverage run -a --source salesforce manage.py test salesforce
 done
-COVERAGE=.tox/py35-dj110/bin/coverage
+COVERAGE=.tox/py36-dj111/bin/coverage
 for x in $(ls -d tests/test_* | sed "s%/%.%"); do
     $COVERAGE run -a --source salesforce manage.py test --settings=$x.settings $x
 done
