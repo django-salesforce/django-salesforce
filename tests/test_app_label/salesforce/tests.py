fcc09829ceec15ed8bb2847777d0bcb188776ada
Hynek Cernoch <hynek@sdb.cz>
2017-08-09 21:17:06 +0200

    Fixed deserialize with app_label config. close #171

---
+++ tests/test_app_label/salesforce/tests.py
@@ -0,0 +1,10 @@
+"""Backward compatible behaviour with primary key 'Id'."""
+from __future__ import absolute_import
+from django.test import TestCase
+from .models import Contact
+
+
+class AppLabelTest(TestCase):
+    def test(self):
+        obj = Contact.objects.all()[0]
+        self.assertEqual(obj._meta.app_label, 'test_salesforce')
---
+++ tests/test_app_label/settings.py
@@ -0,0 +1,7 @@
+"""Test that app config can override a directory name conflict (.e.g. "salesforce")"""
+from __future__ import absolute_import
+from salesforce.testrunner.settings import *  # NOQA
+from salesforce.testrunner.settings import INSTALLED_APPS
+
+INSTALLED_APPS = tuple(x for x in INSTALLED_APPS if x != 'salesforce.testrunner.example')
+INSTALLED_APPS += ('tests.test_app_label.salesforce.apps.TestSalesForceConfig',)
