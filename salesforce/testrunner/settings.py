0e428b1aeea886ddbe56e23b9dd1235f584970ff
Hynek Cernoch <hynek@sdb.cz>
2017-08-17 20:39:32 +0200

    Clarify meaning of 'salesforce' in INSTALLED_APPS

--- salesforce/testrunner/settings.py
+++ salesforce/testrunner/settings.py
@@ -123,6 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'salesforce',
     'salesforce.testrunner.example',
     'salesforce.testrunner.dynamic_models',
 )
