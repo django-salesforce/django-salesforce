e344fb370cfb31c421a75fe598192502a19d69b4
Hynek Cernoch <hynek@sdb.cz>
2018-12-18 03:51:11 +0100

    Fixed If-Unmodifed-Since #210 (#219)
    * Updated coverage measurement script
    * Fixed If-Unmodifed-Since, removed thanks to upstream fix. closed #210

--- coverage_run.sh
+++ coverage_run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # expects that "tox" has been run
-.tox/py36-dj111/bin/coverage run --source salesforce manage.py check >/dev/null
-for x in py27-dj18 py34-dj19 py35-dj110 py36-dj111; do
+COVERAGE=.tox/py37-dj21/bin/coverage
+$COVERAGE run --source salesforce manage.py check >/dev/null
+for x in py27-dj111 py36-dj20 py37-dj21 py35-dj110; do
     .tox/${x}/bin/coverage run -a --source salesforce manage.py inspectdb --database=salesforce >/dev/null
     .tox/${x}/bin/coverage run -a --source salesforce manage.py test salesforce
 done
-COVERAGE=.tox/py36-dj111/bin/coverage
 for x in $(ls -d tests/test_* | sed "s%/%.%"); do
     $COVERAGE run -a --source salesforce manage.py test --settings=$x.settings $x
 done
