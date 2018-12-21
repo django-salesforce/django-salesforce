80c44dc7d71c3b5a7057b53ace60bb9691489869
Hynek Cernoch <hynek@sdb.cz>
2018-12-21 11:25:34 +0100

    Refactored tests/test_*

--- tests/test_lazy_connect/test.sh
+++ tests/test_lazy_connect/test.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-TEST_BASE=tests.
-python manage.py test --settings=tests.test_lazy_connect.settings ${TEST_BASE}test_lazy_connect
+python manage.py test --settings=tests.test_lazy_connect.settings tests.test_lazy_connect
