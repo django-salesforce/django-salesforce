fe248f2d429d0c085f15557331aef536e132249d
Hynek Cernoch <hynek@sdb.cz>
2019-01-13 21:59:50 +0100

    Fixed test for migrations and tests on an alternate database

--- tests/z_migrate_190/test.sh
+++ tests/z_migrate_190/test.sh
@@ -2,22 +2,36 @@
 PROJ=z_migrate_190
 SETTINGS=--settings=tests.$PROJ.settings
 
-python manage.py makemigrations $SETTINGS &&
+# test for Issue 190
+#   "Best way to run in environments without connectivity to Salesforce API? #190
+
+# Test that tables with SalesforceModels
+# - are created in "salesforce" database and
+# - not created in "default" database
+# (if the default SalesforceRouter is used)
+
+python manage.py makemigrations $SETTINGS $PROJ &&
 python manage.py test $SETTINGS tests.$PROJ &&
 python manage.py migrate $SETTINGS --verbosity=0 &&
 python manage.py migrate $SETTINGS --database=salesforce --verbosity=0 &&
 echo .tables | sqlite3 db_tmp_salesforce | grep -w Lead &&
 echo .tables | sqlite3 db_tmp_salesforce | grep -w Contact
 ret=$?
+
 echo .tables | sqlite3 db_tmp_default | grep -w Contact
 ret2=$?
-test $ret -eq 0 -a $ret2 -eq 1
-ret=$?
+
+# delete explicit names to see a warning if they don't exist
 rm db_tmp_default
 rm db_tmp_salesforce
 rm tests/$PROJ/migrations/0001_initial.py
-test
+ret3=$?
+rm -rf tests/$PROJ/migrations/
+
+test $ret -eq 0 -a $ret2 -eq 1 -a $ret3 -eq 0
+ret=$?
+
 if test $ret -ne 0; then
-    echo "Test faied"
+    echo "Test failed"
+    false
 fi
-test $ret -eq 0
