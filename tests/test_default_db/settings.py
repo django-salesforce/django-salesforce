80d5fca5d127582cc78c759f6b889354b70447b6
Hynek Cernoch <hynek@sdb.cz>
2019-06-03 02:15:05 +0200

    Add a test for SALESFORCE_DB_ALIAS=default #231

---
+++ tests/test_default_db/settings.py
@@ -0,0 +1,8 @@
+from salesforce.testrunner.settings import *  # NOQA pylint: disable=unused-wildcard-import,wildcard-import
+from salesforce.testrunner.settings import DATABASES
+
+INSTALLED_APPS = ('tests.test_default_db',)
+DATABASES = {'default': DATABASES['default'], 'salesforce': DATABASES['salesforce']}
+SALESFORCE_DB_ALIAS = 'default'
+ROOT_URLCONF = None
+# SF_LAZY_CONNECT = True
