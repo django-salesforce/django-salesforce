5c31cdb4c86ee87fc2ae4c0242eb06d31b9b2483
Hynek Cernoch <hynek@sdb.cz>
2019-06-15 20:49:12 +0200

    Fixed test with default db for capitalized SF_PK='Id'
    A configurable SF_PK requires to use a corresponding migration.

--- tests/test_default_db/test.sh
+++ tests/test_default_db/test.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-python manage.py test --settings=tests.test_default_db.settings tests.test_default_db
+rm -f tests/test_default_db/migrations/000*
+python manage.py makemigrations --settings=tests.test_default_db.settings test_default_db
+python manage.py test --settings=tests.test_default_db.settings tests.test_default_db && \
+    rm tests/test_default_db/migrations/0001_initial.py*
