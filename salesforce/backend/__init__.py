8f90374d23b5a4f1cdf21058ce5937e94cfe56f8
Hynek Cernoch <hynek@sdb.cz>
2019-09-19 17:28:11 +0200

    Fix Insert for Django 3.0 - different methods with 3.0 and with an older Django

--- salesforce/backend/__init__.py
+++ salesforce/backend/__init__.py
@@ -37,6 +37,7 @@ DJANGO_111_PLUS = django.VERSION[:2] >= (1, 11)
 DJANGO_20_PLUS = django.VERSION[:2] >= (2, 0)
 DJANGO_21_PLUS = django.VERSION[:2] >= (2, 1)
 DJANGO_22_PLUS = django.VERSION[:2] >= (2, 2)
+DJANGO_30_PLUS = django.VERSION[:2] >= (3, 0)
 is_dev_version = django.VERSION[3:] and re.match('(alpha|beta|rc)', django.VERSION[3])
 if django.VERSION[:2] < (1, 10) or django.VERSION[:2] > (2, 2) and not is_dev_version:
     # Usually three or more blocking issues can be expected by every
