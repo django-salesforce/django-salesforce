0809202a4547cd8070fb6808c908332071e391a0
Hynek Cernoch <hynek@sdb.cz>
2019-11-04 20:14:41 +0100

    Changelog; test with Django 3.0b1

--- salesforce/backend/__init__.py
+++ salesforce/backend/__init__.py
@@ -39,7 +39,7 @@ DJANGO_21_PLUS = django.VERSION[:2] >= (2, 1)
 DJANGO_22_PLUS = django.VERSION[:2] >= (2, 2)
 DJANGO_30_PLUS = django.VERSION[:2] >= (3, 0)
 is_dev_version = django.VERSION[3:] and re.match('(alpha|beta|rc)', django.VERSION[3])
-if django.VERSION[:2] < (1, 10) or django.VERSION[:2] > (2, 2) and not is_dev_version:
+if django.VERSION[:2] < (1, 10) or django.VERSION[:2] > (3, 0) and not is_dev_version:
     # Usually three or more blocking issues can be expected by every
     # new major Django version. Strict check before support is better.
 
