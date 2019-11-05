ffb35b9627c9336fd608097552babb1f4ae4dc5c
Phil Christensen <phil@bubblehouse.org>
2019-11-04 22:04:48 -0500

    [ci skip] bump version

--- salesforce/__init__.py
+++ salesforce/__init__.py
@@ -16,7 +16,7 @@ from salesforce.dbapi.exceptions import (  # NOQA pylint:disable=unused-import,u
     IntegrityError as IntegrityError, DatabaseError as DatabaseError, SalesforceError as SalesforceError,
 )
 
-__version__ = "0.8.1post1"
+__version__ = "0.9"
 
 log = logging.getLogger(__name__)
 
