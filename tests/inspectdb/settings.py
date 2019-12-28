5442527e2ffdcb04f6466c9f055dd9356085071a
Hynek Cernoch <hynek@sdb.cz>
2019-12-28 11:50:30 +0100

    Enable Admin site in inspectdb test project

--- tests/inspectdb/settings.py
+++ tests/inspectdb/settings.py
@@ -4,4 +4,4 @@ from salesforce.testrunner.settings import INSTALLED_APPS
 # replace the test app
 INSTALLED_APPS = tuple(x for x in INSTALLED_APPS if not x.startswith('salesforce.testrunner.'))
 INSTALLED_APPS += ('tests.inspectdb',)
-ROOT_URLCONF = None
+ROOT_URLCONF = 'tests.inspectdb.urls'
