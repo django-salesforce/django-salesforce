a7bddc6c25de014f06cd7a191e36c4c0dbce9ab5
Hynek Cernoch <hynek@sdb.cz>
2019-12-30 17:35:35 +0100

    Test insert, update and delete with django-debug-toolbar #247

---
+++ tests/t_debug_toolbar/test.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python manage.py test --settings=tests.t_debug_toolbar.settings tests.t_debug_toolbar
---
+++ tests/t_debug_toolbar/urls.py
@@ -0,0 +1,9 @@
+import debug_toolbar
+from django.urls import include, path
+from salesforce.testrunner.urls import urlpatterns
+from .views import account_insert_delete
+
+urlpatterns += [
+    path('__debug__/', include(debug_toolbar.urls, namespace='djdt')),
+    path('account_insert_delete/', account_insert_delete),
+]
