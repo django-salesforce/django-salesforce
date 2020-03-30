ecc9f79ee7171f56518b15118a2dcb181fe082f1
Hynek Cernoch <hynek@sdb.cz>
2020-03-31 00:59:18 +0200

    Some pylint

--- salesforce/tests/test_doc_tests.py
+++ salesforce/tests/test_doc_tests.py
@@ -1,11 +1,13 @@
-def doc_tests():
-    """Doctests that can be selected by 'manage.py test thismodule.doctests'"""
-    import doctest
-    return doctest.DocTestSuite('salesforce.backend.introspection')
+"""Doctests that can be selected by 'manage.py test thismodule.doctests'"""
+import doctest
+
+doc_tests = [
+    doctest.DocTestSuite('salesforce.backend.introspection'),
+]
 
 
 def load_tests(loader, tests, ignore):
     """Add doctests to unittests"""
     # pylint:disable=invalid-name,unused-argument
-    tests.addTests(doc_tests())
+    tests.addTests(doc_tests)
     return tests
