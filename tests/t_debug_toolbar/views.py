0bc1aa97688611c447e771afd81dd0ba7b8ff5e3
Hynek Cernoch <hynek@sdb.cz>
2020-04-22 15:22:04 +0200

    Fix the test t_debug_toolbar without DEFAULTED_ON_CREATE

--- tests/t_debug_toolbar/views.py
+++ tests/t_debug_toolbar/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
+from salesforce.backend.test_helpers import current_user
 from salesforce.testrunner.example import models
 
 
 def account_insert_delete(request):
     # simple test view for debug-toolbar
-    account = models.Account(Name='abc')
+    user = models.User.objects.get(Username=current_user)
+    account = models.Account(Name='abc', Owner=user)
     account.save()
     account.Name = 'xyz'
     account.save()
