78a6fff6c83eacb281806380a006ecdface38c3b
Hynek Cernoch <hynek@sdb.cz>
2020-04-27 14:24:40 +0200

    Refactored defaults to fix typing and use DetaultedOnCreate in a test again.

--- tests/t_debug_toolbar/views.py
+++ tests/t_debug_toolbar/views.py
@@ -1,12 +1,11 @@
 from django.http import HttpResponse
-from salesforce.backend.test_helpers import current_user
 from salesforce.testrunner.example import models
 
 
 def account_insert_delete(request):
     # simple test view for debug-toolbar
-    user = models.User.objects.get(Username=current_user)
-    account = models.Account(Name='abc', Owner=user)
+    # Owner field is prepared by DefaultedOnCreate in the Account model
+    account = models.Account(Name='abc')
     account.save()
     account.Name = 'xyz'
     account.save()
