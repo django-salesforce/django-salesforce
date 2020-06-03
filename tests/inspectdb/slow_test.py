b0436d11710b9fef5f9875371d2d184ffdda4675
Hynek Cernoch <hynek@sdb.cz>
2020-06-03 02:26:55 +0200

    Refactor inspectdb "slow_test" without changing a functionality

--- tests/inspectdb/slow_test.py
+++ tests/inspectdb/slow_test.py
@@ -34,73 +34,75 @@ sf = connections['salesforce']
 def run():
     start_name = sys.argv[1] if sys.argv[1:] else ''
     n_tables = n_read = n_no_data = n_read_errors = n_write = n_write_errors = 0
+    model_map = {cls._meta.db_table: cls
+                 for cls in mdl.__dict__.values()
+                 if isclass(cls) and issubclass(cls, django.db.models.Model)
+                 }
+    problematic_read = {
+        # These require specific filters (descried in their error messages)
+        'CollaborationGroupRecord', 'ContentFolderMember', 'ContentFolderItem',
+        'ContentDocumentLink', 'Idea', 'IdeaComment', 'UserProfileFeed',
+        'Vote',  # 'OpportunityPartner', 'Product2Feed',
+        # UNKNOWN_EXCEPTION:
+        'TenantUsageEntitlement',
+        }
+    problematic_write = {
+        # Cannot modify managed objects
+        'ApexClass', 'ApexComponent', 'ApexTrigger', 'FieldPermissions',
+        'ObjectPermissions', 'PermissionSet', 'Scontrol',
+        'StaticResource', 'WebLink', 'RecordType',
+        # This is not writable due to 'NamespacePrefix' field
+        'ApexPage',
+        # It does not update the 'last_modified_date' field
+        'AppMenuItem',
+        # Some Leads are not writable becase they are coverted to Contact
+        'Lead',
+        # Insufficient access rights on cross-reference id
+        'Group',
+        'OpportunityShare', 'Profile',
+        # Some very old items can have empty Folder.Name, but can
+        # not be saved again without Name.
+        'Folder',
+        # Records with some values of UserShare.RowCause can not be updated.
+        'UserShare',
+        # Cannot directly insert FeedItem with type TrackedChange
+        'FeedItem',
+        # The Apex Class selected is not valid. An Apex Class that implements
+        # the Messaging.InboundEmailHandler interface must be selected.
+        'EmailServicesFunction',
+        # A special user of type user_type='AutomatedProcess' exists in Users
+        # since API 41.0 Winter'18.
+        # That user account can not be saved because it has an invalid email.
+        'User', 'UserLogin',
+    }
     for tab in sf.introspection.table_list_cache['sobjects']:
-        if tab['retrieveable'] and not tab['name'].endswith('ChangeEvent') and not tab['name'] in (
-                # These require specific filters (descried in their error messages)
-                'CollaborationGroupRecord', 'ContentFolderMember', 'ContentFolderItem',
-                'ContentDocumentLink', 'Idea', 'IdeaComment', 'UserProfileFeed',
-                'Vote',  # 'OpportunityPartner', 'Product2Feed',
-                # TODO The "RecordType" is a very important object, but it can fail
-                # on philchristensen's Salesforce with Travis. It should be more
-                # investigated to which SObject is the RecordType related and enabled
-                # again.
-                'RecordType',
-                # UNKNOWN_EXCEPTION:
-                'TenantUsageEntitlement',
-        ):
-            if tab['name'] < start_name:
+        db_table = tab['name']
+        # More sobjects are 'queryable' and 'retrieveable' than
+        # only 'retrieveable', but half of them would be problematic.
+        if tab['retrieveable'] and not db_table.endswith('ChangeEvent') and db_table not in problematic_read:
+            if db_table < start_name:
                 continue
-            [test_class] = [cls for cls in (getattr(mdl, x) for x in dir(mdl))
-                            if (isclass(cls) and issubclass(cls, django.db.models.Model) and
-                                cls._meta.db_table == tab['name'])
-                            ]
-            stdout.write('%s ' % tab['name'])
+            test_class = model_map[db_table]
+
+            stdout.write('%s' % db_table)
             obj = None
             try:
                 n_read += 1
                 obj = test_class.objects.all()[0]
             except SalesforceError as exc:
-                stderr.write("\n************** %s %s\n" % (tab['name'], exc))
+                stderr.write("\n************** %s %s\n" % (db_table, exc))
                 n_read_errors += 1
             except IndexError:
                 n_no_data += 1
             if obj:
                 stdout.write("* ")
-            if obj and tab['updateable'] and not tab['name'] in (
-                    # Cannot modify managed objects
-                    'ApexClass', 'ApexComponent', 'ApexTrigger', 'FieldPermissions',
-                    'ObjectPermissions', 'PermissionSet', 'Scontrol',
-                    'StaticResource', 'WebLink',
-                    # This is not writable due to 'NamespacePrefix' field
-                    'ApexPage',
-                    # It does not update the 'last_modified_date' field
-                    'AppMenuItem',
-                    # Some Leads are not writable becase they are coverted to Contact
-                    'Lead',
-                    # Insufficient access rights on cross-reference id
-                    'Group',
-                    'OpportunityShare', 'Profile',
-                    # Some very old items can have empty Folder.Name, but can
-                    # not be saved again without Name.
-                    'Folder',
-                    # Records with some values of UserShare.RowCause can not be updated.
-                    'UserShare',
-                    # Cannot directly insert FeedItem with type TrackedChange
-                    'FeedItem',
-                    # The Apex Class selected is not valid. An Apex Class that implements
-                    # the Messaging.InboundEmailHandler interface must be selected.
-                    'EmailServicesFunction',
-                    # A special user of type user_type='AutomatedProcess' exists in Users
-                    # since API 41.0 Winter'18.
-                    # That user account can not be saved because it has an invalid email.
-                    'User', 'UserLogin',
-            ):
+            if obj and tab['updateable'] and db_table not in problematic_write:
                 stdout.write('(write) ')
                 try:
                     n_write += 1
                     obj.save(force_update=True)
                 except SalesforceError as exc:
-                    stderr.write("\n************** %s %s\n" % (tab['name'], exc))
+                    stderr.write("\n************** %s %s\n" % (db_table, exc))
                     n_write_errors += 1
                 else:
                     # object 'Topic' doesn't have the attribute 'last_modified_date'
