8cb63143507cbc9c57ef129d38430555bd6393fa
Hynek Cernoch <hynek@sdb.cz>
2020-06-18 22:32:14 +0200

    Version 1.1

--- salesforce/__init__.py
+++ salesforce/__init__.py
@@ -16,7 +16,7 @@ from salesforce.dbapi.exceptions import (  # NOQA pylint:disable=unused-import,u
     IntegrityError as IntegrityError, DatabaseError as DatabaseError, SalesforceError as SalesforceError,
 )
 
-__version__ = "1.0post1"
+__version__ = "1.1"
 
 log = logging.getLogger(__name__)
 
