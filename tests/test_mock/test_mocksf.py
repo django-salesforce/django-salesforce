a449a5ff6cb25668a0078136cb18cf0089674d57
Hynek Cernoch <hynek@sdb.cz>
2021-04-05 22:28:44 +0200

    Add typing; increase coverage

--- tests/test_mock/test_mocksf.py
+++ tests/test_mock/test_mocksf.py
@@ -2,11 +2,14 @@
 Test that MockTestCase works in all modes, including "record"
 (this must connect to servers)
 """
+import unittest
+from typing import List
 from django.db import connections
 from django.test.utils import override_settings
 
 from salesforce.backend.test_helpers import sf_alias
-from tests.test_mock.mocksf import mock, MockJsonRequest, MockTestCase
+from tests.test_mock.mocksf import (mock, MockJsonRequest, MockTestCase,
+                                    case_safe_sf_id, check_sf_api_id, extract_ids)
 
 
 @override_settings(SF_MOCK_MODE='mixed')
@@ -61,3 +64,41 @@ class TestMock(MockTestCase):
         self.mock_add_expected(req)
         ret = cur.handle_api_exceptions('POST', 'mock:///services/data/')
         self.assertEqual(ret.json()['a'], 3)
+
+
+class SafeIdTest(unittest.TestCase):
+    def test_case_safe_sf_id(self) -> None:
+        self.assertEqual(case_safe_sf_id('000000000000000AAA'), '000000000000000AAA')
+        self.assertEqual(case_safe_sf_id('000000000000000'), '000000000000000AAA')
+        self.assertEqual(case_safe_sf_id('A00000000000000'), 'A00000000000000BAA')
+        self.assertEqual(case_safe_sf_id(None), None)
+        self.assertRaises(TypeError, case_safe_sf_id, '000000000000000AAB')
+        self.assertRaises(TypeError, case_safe_sf_id, '00000000000000@')
+        self.assertRaises(TypeError, case_safe_sf_id, '000000000000')
+
+    def test_check_sf_api_id(self) -> None:
+        self.assertEqual(check_sf_api_id('000000000000000'), '000000000000000AAA')
+        self.assertEqual(check_sf_api_id('0'), None)
+
+    def test_extract_ids(self) -> None:
+        def extract(data, data_type=None) -> List[str]:
+            return [x[0] for x in extract_ids(data, data_type)]
+        json_demo = '{"key": "000000000000000AAA", "name": "something"}'
+        soap_demo = '<key>000000000000000AAA</key><name>something</name>'
+        soql_demo = "WHERE key = '000000000000000AAA' AND name = 'something'"
+
+        self.assertEqual(extract(json_demo), ['000000000000000AAA'])
+        self.assertEqual(extract(soap_demo), ['000000000000000AAA'])
+        self.assertEqual(extract(soql_demo), ['000000000000000AAA'])
+
+        self.assertEqual(extract(json_demo, 'rest'), ['000000000000000AAA'])
+        self.assertEqual(extract(soap_demo, 'rest'), [])
+        self.assertEqual(extract(soql_demo, 'rest'), [])
+
+        self.assertEqual(extract(json_demo, 'soap'), [])
+        self.assertEqual(extract(soap_demo, 'soap'), ['000000000000000AAA'])
+        self.assertEqual(extract(soql_demo, 'soap'), [])
+
+        self.assertEqual(extract(json_demo, 'soql'), [])
+        self.assertEqual(extract(soap_demo, 'soql'), [])
+        self.assertEqual(extract(soql_demo, 'soql'), ['000000000000000AAA'])
