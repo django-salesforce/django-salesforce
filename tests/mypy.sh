43905d66be6546a83f22a017cb31a7d7b31f6798
Hynek Cernoch <hynek@sdb.cz>
2022-07-28 17:46:53 +0200

    Clearer typing script

--- tests/mypy.sh
+++ tests/mypy.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-.tox/typing/bin/mypy --strict salesforce/*.py
+if test -z "$TOX_ENV_DIR"; then
+    echo -e "ERROR must be run from tox\n"
+fi
+MYPY=$TOX_ENV_DIR/bin/mypy
+$MYPY --strict salesforce/*.py
 RET=$?
 MORE_TESTS=tests/inspectdb
+DIRS=
 for x in tests/test_* $MORE_TESTS; do
     if test -e $x/test.sh; then
         if test -e $x/mypy.ini; then
             CONFIG=$x/mypy.ini
+            echo "== .$MYPY --config-file=$CONFIG $x $@ =="
+            $MYPY --config-file=$CONFIG $x $@ || RET=$?
         else
-            CONFIG=mypy.ini
+            DIRS="$DIRS $x"
         fi
-	echo "== .tox/typing/bin/mypy --config-file=$CONFIG $x $@ =="
-        .tox/typing/bin/mypy --config-file=$CONFIG $x $@ || RET=$?
     fi
 done
+CONFIG=mypy.ini
+echo "== $MYPY --config-file=$CONFIG $DIRS $@ =="
+$MYPY --config-file=$CONFIG $DIRS  $@ || RET=$?
 test $RET -eq 0
