38524fb6e4c8f032cf8442158d05118e799e0a4f
Hynek Cernoch <hynek@sdb.cz>
2023-05-31 13:07:14 +0200

    Add validation "python manage.py check --database=salesforce"

--- salesforce/backend/validation.py
+++ salesforce/backend/validation.py
@@ -7,8 +7,62 @@
 """
 Default validation code.  (like django.db.backends.*.validation)
 """
+from typing import Any, List
+from django.core.management import ManagementUtility
+from django.core import checks
+from django.db import connections, router as django_router
 from django.db.backends.base.validation import BaseDatabaseValidation
+from requests.exceptions import ConnectionError
+from salesforce.dbapi.exceptions import LicenseError, SalesforceError
+from salesforce.models import SalesforceModel
+from salesforce.router import is_sf_database
 
 
 class DatabaseValidation(BaseDatabaseValidation):
-    pass
+    def check(self, **kwargs) -> List[Any]:
+        issues = super().check(**kwargs)
+        issues.extend(self.check_standard_install(**kwargs))
+        return issues
+
+    def check_standard_install(self, **kwargs) -> List[Any]:
+        issues = []
+
+        alias = self.connection.alias
+        try:
+            connections[alias].cursor()
+        except LicenseError:
+            issues.append(
+                checks.Warning(
+                    "DJSF_LICENSE_KEY is necessary for django-salesforce with the newest Django version",
+                    hint='You can become a sponsor to get it or you can use our equivalent package '
+                    "django-salesforce-agpl if an AGPL license is acceptable for you.",
+                    id="salesforce.W005",
+                )
+            )
+        except (SalesforceError, ConnectionError) as exc:
+            issues.append(
+                checks.Warning(
+                    f"Can not connect to a Salesforce database '{alias}'",
+                    hint=repr(exc),
+                )
+            )
+
+        if not is_sf_database(django_router.db_for_read(SalesforceModel)):
+            issues.append(
+                checks.Warning(
+                    "Tables with SalesforceModel are not routed to a Salesforce database",
+                    hint='add settings DATABASE_ROUTERS = ["salesforce.router.ModelRouter"]  # (README 3)',
+                    id="salesforce.W003",
+                )
+            )
+
+        if 'salesforce' not in ManagementUtility(['', '']).fetch_command('inspectdb').__module__:
+            issues.append(
+                checks.Warning(
+                    "The command 'inspectdb' would not work correctly with Salesforce databases",
+                    hint='add "salesforce" to INSTALLED_APPS (README 4)',
+                    id="salesforce.W004",
+                )
+            )
+
+        return issues
