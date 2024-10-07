27db792be5ce7299f7786545380a22c912990f2f
Hynek Cernoch <hynek@sdb.cz>
2024-10-07 08:52:58 +0200

    Modernize and simplify the distibution by pyproject.toml; dynamic dependency remains in setup.py

--- setup.py
+++ setup.py
@@ -1,79 +1,12 @@
-# django-salesforce
-#
-# by Hyneck Cernoch and Phil Christensen
-# See LICENSE.md for details
-#
-
+# minimal dynamic metadata are specified here. Static metadata are in "pyproject.toml".
 import os
-import re
+import setuptools
 
-# disables creation of .DS_Store files inside tarballs on Mac OS X
-os.environ['COPY_EXTENDED_ATTRIBUTES_DISABLE'] = 'true'
-os.environ['COPYFILE_DISABLE'] = 'true'
-
-
-def relative_path(path):
-    """
-    Return the given path relative to this file.
-    """
-    return os.path.join(os.path.dirname(__file__), path)
-
-
-def get_tagged_version():
-    """
-    Determine the current version of this package.
-
-    Precise long version numbers are used if the Git repository is found.
-    They contain: the Git tag, the commit serial and a short commit id.
-    otherwise a short version number is used if installed from Pypi.
-    """
-    with open(relative_path('salesforce/__init__.py'), 'r') as fd:
-        version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                            fd.read(), re.MULTILINE).group(1)
-    return version
-
-
-def autosetup():
-    from setuptools import setup, find_packages
-
-    sf_development = os.path.isfile('.djsf_development')
-
-    with open(relative_path('README.rst'), 'r', newline=None) as f:
-        long_description = f.read()
-
-    # check if installed with git data (not via PyPi)
-    with_git = os.path.isdir(relative_path('.git'))
-
-    return setup(
-        name="django-salesforce",
-        version=get_tagged_version(),
-
-        include_package_data=True,
-        zip_safe=False,
-        packages=find_packages(exclude=['tests', 'tests.*']),
-
-        # setuptools won't auto-detect Git managed files without this
-        setup_requires=[] if not with_git else ["setuptools_git >= 0.4.2"],
-
-        install_requires=[
-            'django>=2.1' + (',<5.2' if not sf_development else ''),
-            'pytz>=2012c',
-            'requests>=2.32.0',
-        ],
-
-        # metadata for upload to PyPI
-        author="Hynek Cernoch",
-        author_email="hynek@sdb.cz",
-        maintainer="Phil Christensen",
-        maintainer_email="phil@bubblehouse.org",
-        description="a Salesforce backend for Django's ORM",
-        long_description=long_description,
-        long_description_content_type="text/x-rst",
-        license="AGPL",
-        keywords="django salesforce orm backend",
-        url="https://github.com/django-salesforce/django-salesforce",
-    )
-
-
-if __name__ == '__main__':
-    dist = autosetup()
+sf_development = os.path.isfile('.djsf_development')
+setuptools.setup(
+    install_requires=[
+        'django>=2.1' + (',<5.2' if not sf_development else ''),
+        'pytz>=2012c',
+        'requests>=2.32.0',
+    ],
+)
