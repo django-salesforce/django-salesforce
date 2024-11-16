9473abcc1ed0f81ca6dce662a5bd1e824893542f
Hynek Cernoch <hynek@sdb.cz>
2024-11-16 16:56:54 +0100

    Better test that a development Django version is used with djdev

--- salesforce/tests/test_basic.py
+++ salesforce/tests/test_basic.py
@@ -13,12 +13,14 @@ class VersionTest(TestCase):
         e.g. if versions required by tox deps or setup.py are in conflict.
         The unexpected version should be immediately uninstalled and replaced by a requiered version.
         """
-        match = re.search(r'/\.tox/dj(\d+|dev)-py(\d+)/', django.__file__)
+        match = re.search(r'/\.tox/dj(\d+|dev)(?:-py(\d+))?/', django.__file__)
         if match:
             django_version_abbr, python_version_abbr = match.groups()
             if django_version_abbr == 'dev':
-                self.assertTrue(backend.is_dev_version, "This should be a test environment of a DEV version")
+                self.assertTrue(backend.is_dev_version, "a DEVELOPMENT version of Django should be installed "
+                                "in this DEV environment")
                 self.assertGreater(django.VERSION[:2], backend.max_django)
             else:
                 self.assertEqual('{}{}'.format(*django.VERSION[:2]), django_version_abbr)
-            self.assertEqual('{}{}'.format(*sys.version_info[:2]), python_version_abbr)
+            if python_version_abbr is not None:
+                self.assertEqual('{}{}'.format(*sys.version_info[:2]), python_version_abbr)
